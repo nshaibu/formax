361880e1034a816aeff2ae37327b3de5234f1164
nshaibu <nafiushaibu1@gmail.com>
2026-02-04 01:39:56 +0000

    add benchmark for pydantic mini against dc

---
+++ benchmark/benchmark_model_eff.py
@@ -0,0 +1,33 @@
+import json
+from .models import (
+    UserDC,
+    UserMini,
+    UserNestedDC,
+    UserNestedMini,
+    ProfileDC,
+)
+
+
+def test_flat_dataclass(benchmark, data):
+    benchmark(lambda: UserDC(**data))
+
+
+def test_flat_pydantic_mini(benchmark, data):
+    benchmark(lambda: UserMini(**data))
+
+
+def test_nested_dataclass(benchmark, nested_data):
+    benchmark(
+        lambda: UserNestedDC(
+            id=nested_data["id"],
+            name=nested_data["name"],
+            profile=ProfileDC(**nested_data["profile"]),
+        )
+    )
+
+def test_nested_pydantic_mini(benchmark, nested_data):
+    benchmark(lambda: UserNestedMini(**nested_data))
+
+
+def test_pydantic_mini_json(benchmark, json_data):
+    benchmark(lambda: UserMini.loads(json_data, _format="json"))
---
+++ benchmark/models.py
@@ -0,0 +1,65 @@
+import json
+from dataclasses import dataclass
+from typing import List
+
+from pydantic_mini import BaseModel
+
+
+DATA = {
+    "id": 123,
+    "name": "Alice",
+    "scores": [10, 20, 30],
+    "active": True,
+}
+
+NESTED_DATA = {
+    "id": 123,
+    "name": "Alice",
+    "profile": {
+        "email": "alice@example.com",
+        "age": 30,
+    },
+}
+
+JSON_DATA = json.dumps(DATA)
+
+
+@dataclass
+class ProfileDC:
+    email: str
+    age: int
+
+
+@dataclass
+class UserDC:
+    id: int
+    name: str
+    scores: List[int]
+    active: bool
+
+
+@dataclass
+class UserNestedDC:
+    id: int
+    name: str
+    profile: ProfileDC
+
+
+
+class ProfileMini(BaseModel):
+    email: str
+    age: int
+
+
+class UserMini(BaseModel):
+    id: int
+    name: str
+    scores: List[int]
+    active: bool
+
+
+class UserNestedMini(BaseModel):
+    id: int
+    name: str
+    profile: ProfileMini
+
