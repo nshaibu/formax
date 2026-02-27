15028c4166e8ebb6f9863aaef8ca045ff9515dbe
nshaibu <nafiushaibu1@gmail.com>
2026-02-27 02:56:06 +0000

    project named to formax

--- benchmark/benchmark_model_eff.py
+++ benchmark/benchmark_model_eff.py
@@ -18,7 +18,7 @@ def test_flat_dataclass(benchmark, data):
     benchmark(lambda: UserDC(**data))
 
 
-def test_flat_pydantic_mini(benchmark, data):
+def test_flat_formax(benchmark, data):
     benchmark(lambda: UserMini(**data))
 
 
@@ -32,27 +32,27 @@ def test_nested_dataclass(benchmark, nested_data):
     )
 
 
-def test_nested_pydantic_mini(benchmark, nested_data):
+def test_nested_formax(benchmark, nested_data):
     benchmark(lambda: UserNestedMini(**nested_data))
 
 
-def test_nested_disable_all_validation_pydantic_mini(benchmark, nested_data):
+def test_nested_disable_all_validation_formax(benchmark, nested_data):
     benchmark(lambda: DisableAllValidationMini(**nested_data))
 
 
-def test_nested_disable_type_check_pydantic_mini(benchmark, nested_data):
+def test_nested_disable_type_check_formax(benchmark, nested_data):
     benchmark(lambda: DisableTypeCheckMini(**nested_data))
 
 
-def test_flat_disable_all_validation_pydantic_mini(benchmark, data):
+def test_flat_disable_all_validation_formax(benchmark, data):
     benchmark(lambda: FlatDisableAllValidationProfileMini(**data))
 
 
-def test_flat_disable_type_check_pydantic_mini(benchmark, data):
+def test_flat_disable_type_check_formax(benchmark, data):
     benchmark(lambda: FlatDisableTypeCheckProfileMini(**data))
 
 
-def test_pydantic_mini_json(benchmark, json_data):
+def test_formax_json(benchmark, json_data):
     benchmark(lambda: UserMini.loads(json_data, _format="json"))
 
 
